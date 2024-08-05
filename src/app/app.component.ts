bed43dd1bc77c4cdb78014446e526d84d083cf7d
Zeyad-Awad-Abdelazim <130605801+Zeyad-Awad-Abdelazim@users.noreply.github.com>
2024-08-05 15:23:46 +0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FooterComponent } from './Components/footer/footer.component';
+import { HeroComponent } from './Components/hero/hero.component';
+import { BioComponent } from './Components/bio/bio.component';
+import { SkillsComponent } from './Components/skills/skills.component';
+import { ProjectsComponent } from './Components/projects/projects.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeroComponent, BioComponent, SkillsComponent, ProjectsComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
